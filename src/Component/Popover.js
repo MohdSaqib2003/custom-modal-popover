877b444c950dde8049425493f4049a6e1e2d8d83
Mohd Saqib <mohd.saqib@sita.in>
2024-09-21 17:33:18 +0530

    On outside click modal will be closed

--- src/Component/Popover.js
+++ src/Component/Popover.js
@@ -4,17 +4,32 @@ import "./index.css";
 const Popover = (props) => {
   const { visible, setVisible, children } = props;
   return visible ? (
-    <div className="popover-container">
-      <div className="inner-popover">
+    <div
+      className="popover-container"
+      onClick={(e) => {
+        // close the modal on outside click
+        setVisible(false);
+      }}
+    >
+      <div
+        className="inner-popover"
+        onClick={(e) => {
+          // Restrict closing the modal by clicking on the modal itself
+          setVisible(true);
+          e.stopPropagation(); // It restricts the execution of the parent's event
+        }}
+      >
         <div className="header">
           <div>
             <b> Modal </b>
-          </div> <hr />
+          </div>{" "}
+          <hr />
           <div>
             <button
               onClick={(e) => {
                 // Close the modal on click on close btn
                 setVisible(false);
+                e.stopPropagation(); //It restricts the execution of the parent's event
               }}
             >
               close x
